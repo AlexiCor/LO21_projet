57a9f7f43fd39c48bb501ed6d8932bbdc8729aa6
AlexiCor <correard.alexis@gmail.com>
2023-12-12 14:09:59 +0100

    Implémentation des fonctions creerRegleVide et creerBaseVide dans initialisation.c et initialisation.h

--- initialisation.c
+++ initialisation.c
@@ -1,4 +1,23 @@
 #include "initialisation.h"
+#include "declaration.h"
+
+
+Proposition creerRegleVide(){
+    Proposition nouvelleProp;
+    nouvelleProp.suivant = NULL;
+    nouvelleProp.idProposition = NULL;
+    return nouvelleProp;
+}
+
+Regle creerBaseVide(){
+    Regle nouvelleRegle;
+    nouvelleRegle.suivant = NULL;
+    nouvelleRegle.LProposition = NULL;
+    return nouvelleRegle;
+}
+
+
+
 
 
 
--- initialisation.h
+++ initialisation.h
@@ -2,4 +2,9 @@
 #ifndef PROJETLO21_INITIALISATION_H
 #define PROJETLO21_INITIALISATION_H
 
+#include "declaration.h"
+
+Proposition creerRegleVide();
+Regle creerBaseVide();
+
 #endif //PROJETLO21_INITIALISATION_H
