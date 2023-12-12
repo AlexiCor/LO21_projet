c37de13ad585ff3e35e97068975bf749e07a8bcf
aurellyo <aurelien.duvet@utbm.fr>
2023-12-12 13:50:21 +0100

    j'ai rajouté include stdlib

--- declaration.h
+++ declaration.h
@@ -1,5 +1,19 @@
-
 #ifndef LO21_PROJET_DECLARATION_H
 #define LO21_PROJET_DECLARATION_H
+
+typedef struct Proposition{
+    int idProposition;
+    struct Proposition *suivant;
+}Proposition
+typedef Proposition *listeProposition;
+
+typedef struct Regle{
+    listeProposition LProposition;
+    struct Regle *suivant;
+}Regle;
+typedef Regle *BC;
+
+
+
 
 #endif //LO21_PROJET_DECLARATION_H
