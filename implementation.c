478737d50ef7296388fa3985c0606a248456936d
AlexiCor <correard.alexis@gmail.com>
2023-12-17 16:48:08 +0100

    Changes by aurel

--- implementation.c
+++ implementation.c
@@ -1,17 +1,40 @@
 #include "implementation.h"
 #include "declaration.h"
+#include "verification.h"
 
 listeProposition ajoutProposition(int e, listeProposition l) {
-    Proposition *nouvelleProp = creerRegleVide();
-        nouvelleProp->idProposition = e;
+    Proposition nouvelleProp = creerRegleVide();
+    nouvelleProp->idProposition = e;
 
-        if (videPremisse(l) != TRUE) {
-
+        if (!videPremisse(l)) {
             conclusionRegle(l)->suivant = nouvelleProp;
         } else {
             l = nouvelleProp;
         }
 
 
+    return l;
+}
+
+listeProposition ajoutConlusion(int e, listeProposition l) {
+   if(!videPremisse(l)){
+       Proposition nouvelleProp = creerRegleVide();
+       nouvelleProp->idProposition = e;
+       conclusionRegle(l)->suivant=nouvelleProp;
+   }
+
+    return l;
+}
+
+BC ajoutRegle(Regle r, BC l){
+    if(videBase(l)){
+        l=r;
+    }else{
+        Regle p = teteBase(l);
+        while(videPremisse(suivant(p))==0){
+            p=suivant(p);
+        }
+        r=suivant(p);
+    }
     return l;
 }
--- implementation.h
+++ implementation.h
@@ -3,6 +3,10 @@
 #ifndef PROJETLO21_IMPLEMENTATION_H
 #define PROJETLO21_IMPLEMENTATION_H
 
+#include "declaration.h"
+
 listeProposition ajoutProposition(int e, listeProposition l);
+listeProposition ajoutConlusion(int e, listeProposition l);
+BC ajoutRegle(Regle r, BC l);
 
 #endif //PROJETLO21_IMPLEMENTATION_H
